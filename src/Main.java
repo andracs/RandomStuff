8cf778080b14cdad36c0340aaa42d42bdbfb60b3
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2018-12-11 12:01:37 +0100

    DOCUMENTATION

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
 import java.lang.reflect.Array;
 import java.util.Scanner;
 
+/**
+* Det her er en hurtig test af javadoc dokumentation
+*/
+
+
 public class Main {
 
     public static void main(String[] args) {
+        /**
+         * Her instantieres variablerne a og b til et random 2 cifret tal og printer
+         */
         int a = (int)(Math.random() * 100);
         int b = (int)(Math.random() * 100);
         System.out.println("Adding quiz!");
         System.out.println("What is: " + a + " + " + b + " ?");
 
+        /**
+         * her bruges en scanner til at spørge brugeren om svaret
+         */
+
         Scanner input = new Scanner(System.in);
+
+        /**
+         * Her er loopet som finder ud af om svaret er rigtigt og enten ender programmet hvis det er rigtigt
+         * eller starter loopet forfra hvis svaret er forkert
+         */
 
         int answer = input.nextInt();
         while (answer != a+b) {
